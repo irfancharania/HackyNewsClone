b44cc749d106620826983fb020e93ba4e7fc66be
Adam Bard <adam@adambard.com>
2017-07-27 22:52:03 -0700

    Got web project workign and added Models to domain

---
+++ HackyNewsWeb/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using HackyNewsDomain;
+
+namespace HackyNewsWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var settings = new Data.Settings();
+            var feed = new Models.Feed(settings);
+
+            return View(feed);
+        }
+    }
+}
